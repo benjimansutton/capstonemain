bd122ea5c91472a4fa4f6f8d4c29f337701ed242
benjimansutton <benjimansutton@gmail.com>
2022-10-18 13:08:24 +0100

    Mid Day Commit

--- src/test/java/com/benjiman/capstone/CapstoneApplicationTests.java
+++ src/test/java/com/benjiman/capstone/CapstoneApplicationTests.java
@@ -3,11 +3,23 @@ package com.benjiman.capstone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static com.benjiman.capstone.CapstoneApplication.troops;
+
 @SpringBootTest
 class CapstoneApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		// Test for the Grab XML
+		System.out.println(XMLData.GrabXML());
+
+		// Test for Sort Ammo with the descending as default
+		System.out.println(SortClass.sortAmmo(troops, "desc"));
+
+		// Test for Sort Percentage with the descending as default
+		System.out.println(SortClass.sortPerc(troops, "desc"));
+
 	}
 
 }
